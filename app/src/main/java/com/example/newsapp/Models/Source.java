7f437cf4b81ac1f70522119dff5be96c861c053f
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-08 20:39:53 +0100

    create source model

---
+++ app/src/main/java/com/example/newsapp/Models/Source.java
@@ -0,0 +1,19 @@
+package com.example.newsapp.Models;
+
+public class Source {
+    private String id;
+    private String name;
+
+    public Source(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
