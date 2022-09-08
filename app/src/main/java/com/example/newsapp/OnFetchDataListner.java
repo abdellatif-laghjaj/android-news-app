ada5a9e798d578fa6fff19a85dad5272509c6cd8
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-08 21:27:22 +0100

    fetch data from api

---
+++ app/src/main/java/com/example/newsapp/OnFetchDataListner.java
@@ -0,0 +1,4 @@
+package com.example.newsapp;
+
+public interface OnFetchDataListner {
+}
---
+++ app/src/main/java/com/example/newsapp/RequestManager.java
@@ -0,0 +1,33 @@
+package com.example.newsapp;
+
+import android.content.Context;
+
+import com.example.newsapp.Models.NewsApiResponse;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public class RequestManager {
+    private Context context;
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public RequestManager(Context context) {
+        this.context = context;
+    }
+
+    public interface CallNewsApi {
+        @GET("top-headlines")
+        Call<NewsApiResponse> callHeadlines(
+                @Query("country") String country,
+                @Query("category") String category,
+                @Query("q") String query,
+                @Query("apiKey") String apiKey
+        );
+    }
+}
