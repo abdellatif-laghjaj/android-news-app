9800f1d8c517d0e3062290ef7e4499f9916ed539
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-08 23:48:07 +0100

    show details

--- app/src/main/java/com/example/newsapp/DetailsActivity.java
+++ app/src/main/java/com/example/newsapp/DetailsActivity.java
@@ -3,11 +3,16 @@ package com.example.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.newsapp.Models.Article;
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
     Article article;
+    TextView textTitle, textDescription, textContent, textAuthor, textPublishedAt;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,22 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         article = (Article) getIntent().getSerializableExtra("data");
+        textTitle = findViewById(R.id.text_details_title);
+        textDescription = findViewById(R.id.text_details_details);
+        textContent = findViewById(R.id.text_details_content);
+        textAuthor = findViewById(R.id.text_details_author);
+        textPublishedAt = findViewById(R.id.text_details_time);
+        imageView = findViewById(R.id.img_details_image);
+
+        setData();
+    }
+
+    private void setData() {
+        textTitle.setText(article.getTitle());
+        textDescription.setText(article.getDescription());
+        textContent.setText(article.getContent());
+        textAuthor.setText(article.getAuthor());
+        textPublishedAt.setText(article.getPublishedAt());
+        Picasso.get().load(article.getUrlToImage()).into(imageView);
     }
 }
