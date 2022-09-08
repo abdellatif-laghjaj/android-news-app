de5a661b339c6eabcbdc5fd8a64901350e7c13c1
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-08 21:09:59 +0100

    fetch views

--- app/src/main/java/com/example/newsapp/ArticleAdapter.java
+++ app/src/main/java/com/example/newsapp/ArticleAdapter.java
@@ -1,12 +1,14 @@
 package com.example.newsapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.newsapp.Models.Article;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -22,16 +24,18 @@ public class ArticleAdapter extends RecyclerView.Adapter<CustomViewHolder> {
     @NonNull
     @Override
     public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new CustomViewHolder(LayoutInflater.from(context).inflate(R.layout.headline_list_items, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
-
+        holder.text_title.setText(articles.get(position).getTitle());
+        holder.text_source.setText(articles.get(position).getSource().getName());
+        Picasso.get().load(articles.get(position).getUrlToImage()).into(holder.img_headline);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return articles.size();
     }
 }
