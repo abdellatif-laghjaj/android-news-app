8da93a24498a7a81e3102082439514b2c4fdb791
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-08 20:43:29 +0100

    create article model

---
+++ app/src/main/java/com/example/newsapp/Models/Article.java
@@ -0,0 +1,55 @@
+package com.example.newsapp.Models;
+
+public class Article {
+    private Source source;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+
+    public Article(Source source, String author, String title, String description, String url, String urlToImage, String publishedAt, String content) {
+        this.source = source;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+    }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
