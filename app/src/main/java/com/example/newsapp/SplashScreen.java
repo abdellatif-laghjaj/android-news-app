f6c74882107aa9e6f279ebb52bcf121bf606659b
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-09 01:58:43 +0100

    finish

---
+++ app/src/main/java/com/example/newsapp/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //remove action bar
+        getSupportActionBar().hide();
+
+        // remove title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
+        }, 5000);
+    }
+}
